d64da52cb7370870e73ce0e270635d19824c3c68
Lpmacau <Periodico@192.168.1.66>
2015-09-17 18:35:25 +0100

    Print

--- SI - Jade Learning/src/Learning1/main.java
+++ SI - Jade Learning/src/Learning1/main.java
@@ -1,10 +1,13 @@
 package Learning1;
 
+import jade.core.Agent;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Agent a = new Agent();
+		System.out.println(a.getLocalName());
 	}
 
 }
