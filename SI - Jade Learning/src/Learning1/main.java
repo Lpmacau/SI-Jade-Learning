60fb0bd485bbbaa7d4bca9a2ca8cb58713b20568
Lpmacau <Periodico@172.26.61.4>
2015-09-21 09:10:26 +0100

    Pré-aula 21/09

--- SI - Jade Learning/src/Learning1/main.java
+++ SI - Jade Learning/src/Learning1/main.java
@@ -5,9 +5,6 @@ import jade.core.Agent;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Agent a = new Agent();
-		System.out.println(a.getLocalName());
 	}
 
 }
